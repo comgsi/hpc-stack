2881ee2a5d534105e220771f93751aa6ac7a98e9
guoqing.ge <Guoqing.Ge@noaa.gov>
2021-05-06 14:38:39 -0600

    for jet

--- config/config_comgsi.sh
+++ config/config_comgsi.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Compiler/MPI combination
-export HPC_COMPILER="intel/18.0.5"
+export HPC_COMPILER="intel/18.0.5.274"
 export HPC_MPI="impi/2018.4.274"
 
 # Build options
 export USE_SUDO=N
 export PKGDIR=pkg
 export LOGDIR=log
-export OVERWRITE=N
+export OVERWRITE=Y
 export NTHREADS=4
 export   MAKE_CHECK=N
 export MAKE_VERBOSE=N
